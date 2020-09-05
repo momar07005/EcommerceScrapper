83eff001205e2cb9dcc456383a59d12cdb09c285
Papa Momar Nokho <papamomarnokho@hotmail.com>
2020-09-05 23:10:07 +0100

    Add Collector service to application layer

---
+++ Application/Services/CollectorService.cs
@@ -0,0 +1,29 @@
+﻿using Application.Data.DTO;
+using Domain.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Services
+{
+    public class CollectorService : ICollectorService
+    {
+        private IScrapper _scrapper;
+
+        public CollectorService(IScrapper scrapper)
+        {
+            _scrapper = scrapper ?? throw new ArgumentNullException(nameof(scrapper));
+        }
+
+        public Task<List<ResponseDTO>> CollectMultipleProductsReviews(CollectReviewsBulkRequestDTO bulkRequestDTO)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResponseDTO> CollectSingleProductReviews(CollectReviewsSingleRequestDTO requestDTO)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Application/Services/ICollectorService.cs
@@ -0,0 +1,18 @@
+﻿using Domain.Entities;
+using Application.Data;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Application.Data.DTO;
+using System.Threading.Tasks;
+
+namespace Application.Services
+{
+    public interface ICollectorService
+    {
+        public Task<ResponseDTO> CollectSingleProductReviews(CollectReviewsSingleRequestDTO requestDTO);
+
+        public Task<List<ResponseDTO>> CollectMultipleProductsReviews(CollectReviewsBulkRequestDTO bulkRequestDTO);
+        
+    }
+}
