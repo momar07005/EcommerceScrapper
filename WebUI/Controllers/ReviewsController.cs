8873c084b4af87aea48b18b79c683b2ba4ed3b8f
Papa Momar Nokho <papamomarnokho@hotmail.com>
2020-09-07 02:08:29 +0100

    Add ReviewsController with single product indexation request action

---
+++ WebUI/Controllers/ReviewsController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Application.Data.DTO;
+using Application.Services;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebUI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReviewsController : ControllerBase
+    {
+        private ICollectorService _collectorService { get; set; }
+
+        public ReviewsController(ICollectorService collectorService)
+        {
+            _collectorService = collectorService ?? throw new ArgumentNullException(nameof(collectorService));
+        }
+
+        // POST: api/<ReviewsController>
+        [HttpPost]
+        public ResponseDTO Post([FromBody] CollectReviewsSingleRequestDTO collectReviewsRequest)
+        {
+            ResponseDTO response = new ResponseDTO();
+            if (ModelState.IsValid)
+            {
+                response = _collectorService.CollectSingleProductReviews(collectReviewsRequest).Result;
+            }
+            return response;
+        }
+    }
+}
