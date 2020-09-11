bec76e1c1cd61876cd9c75c122b06ad501277bf8
Papa Momar Nokho <papamomarnokho@hotmail.com>
2020-09-11 12:45:36 +0100

    Add RequestRepository

---
+++ Domain/Repositories/IRequestRepository.cs
@@ -0,0 +1,18 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Repositories
+{
+    public interface IRequestRepository
+    {
+        public Request Add(Request request);
+
+        public List<Request> GetAllRequest();
+
+        List<Request> GetRequestsByProductId(string productId);
+
+        public Request Update(Request request);
+    }
+}
---
+++ Infrastructure/Persistence/Repositories/RequestRepository.cs
@@ -0,0 +1,59 @@
+﻿using Domain.Entities;
+using Domain.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Infrastructure.Persistence.Repositories
+{
+    public class RequestRepository : IRequestRepository
+    {
+        private readonly ScrappingContext _scrappingContext;
+
+        public RequestRepository(ScrappingContext scrappingContext)
+        {
+            _scrappingContext = scrappingContext ?? throw new ArgumentNullException(nameof(scrappingContext));
+        }
+
+        public Request Add(Request request)
+        {
+           if(request != null)
+           {
+                request = _scrappingContext.Requests.Add(request).Entity;
+                _scrappingContext.SaveChanges();
+           }
+
+           return request;
+        }
+
+        public List<Request> GetAllRequest()
+        {
+            return _scrappingContext.Requests.ToList() ;
+        }
+
+        public List<Request> GetRequestsByProductId(string productId)
+        {
+            if(productId != null)
+            {
+                return _scrappingContext.Requests.Where(request => request.ProductId == productId).ToList();
+            }
+            else
+            {
+                return new List<Request>();
+            }
+            
+        }
+
+        public Request Update(Request request)
+        {
+            if (request != null && request.Id > 0)
+            {
+                request = _scrappingContext.Requests.Attach(request).Entity;
+                _scrappingContext.SaveChanges();
+            }
+
+            return request;
+        }
+    }
+}
