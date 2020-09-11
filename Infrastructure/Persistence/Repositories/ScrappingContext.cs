30b787c7f3298bddc37b97effd1dd1642de19f1c
Papa Momar Nokho <papamomarnokho@hotmail.com>
2020-09-11 12:37:35 +0100

    Add EntityFrameworkCore and Scrapping BD context

---
+++ Infrastructure/Persistence/Repositories/ScrappingContext.cs
@@ -0,0 +1,24 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Infrastructure.Persistence.Repositories
+{
+    public class ScrappingContext : DbContext
+    {
+        public ScrappingContext(DbContextOptions<ScrappingContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Request> Requests { get; set; }
+        
+        public DbSet<Review> Reviews { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Review>().HasKey(review => review.ReviewId);
+        }
+    }
+}
