3bd76b02fed2ca12e6fb2166a74a3852c725422e
Papa Momar Nokho <papamomarnokho@hotmail.com>
2020-09-14 23:03:29 +0100

    Declare new components and modules into appModule

--- WebUI/ClientApp/src/app/app.module.ts
+++ WebUI/ClientApp/src/app/app.module.ts
@@ -3,14 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { RequestComponent } from './request/request.component';
+import { ReviewComponent } from './review/review.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RequestComponent,
+    ReviewComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatTableModule,
+    MatPaginatorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
