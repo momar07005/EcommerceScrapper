66c9a20951a87917ed1b9203be9953bda8e10016
Papa Momar Nokho <papamomarnokho@hotmail.com>
2020-09-14 23:18:57 +0100

    Add get request route

--- WebUI/ClientApp/src/app/app-routing.module.ts
+++ WebUI/ClientApp/src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { RequestComponent } from './request/request.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: RequestComponent },
+  { path: 'requests', component: RequestComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
