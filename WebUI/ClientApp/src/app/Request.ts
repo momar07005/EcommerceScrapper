d157b62b84993c6294a696236be4b23c3158447f
Papa Momar Nokho <papamomarnokho@hotmail.com>
2020-09-16 00:48:40 +0100

    Add BulkRequest class

--- WebUI/ClientApp/src/app/Request.ts
+++ WebUI/ClientApp/src/app/Request.ts
@@ -1,6 +1,28 @@
-export class Request {
-  productId: string;
+import { RequestStatusEnum } from './RequestStatusEnum';
+
+class BaseRequest {
+  id: number;
   numberOfReviews: number;
   status: RequestStatusEnum;
   date: Date;
+
+  constructor(numberOfReviews: number, date: Date, status: RequestStatusEnum = RequestStatusEnum.Ongoing) {
+    this.numberOfReviews = numberOfReviews;
+    this.date = date;
+    this.status = status;
+  } 
+
+}
+
+export class Request extends BaseRequest{
+  productId: string;
+
+  constructor(productId: string, numberOfReviews: number, date: Date) {
+    super(numberOfReviews, date);
+    this.productId = productId;
+  } 
+}
+
+export class BulkRequest extends BaseRequest{
+  productIds: string[];
 }
