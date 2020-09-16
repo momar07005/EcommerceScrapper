aec7c3a2ca0afee7e41c1074ddb5cc8b426c4be8
Papa Momar Nokho <papamomarnokho@hotmail.com>
2020-09-17 00:29:46 +0100

    Add reviews display feature

--- WebUI/ClientApp/src/app/app-routing.module.ts
+++ WebUI/ClientApp/src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { RequestComponent } from './request/request.component';
+import { ReviewComponent } from './review/review.component';
 
 
 const routes: Routes = [
-  { path: '', component: RequestComponent },
+  { path: '', redirectTo: '/requests', pathMatch: 'full' },
   { path: 'requests', component: RequestComponent },
+  { path: 'reviews', component: ReviewComponent },
 ];
 
 @NgModule({
--- WebUI/ClientApp/src/app/review/review.component.ts
+++ WebUI/ClientApp/src/app/review/review.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { RepositoryService } from '../repository.service';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatPaginator } from '@angular/material/paginator';
+import { Review } from '../Review';
 
 @Component({
   selector: 'app-review',
@@ -7,9 +13,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReviewComponent implements OnInit {
 
-  constructor() { }
+  reviews: Observable<Review[]>;
+
+  public displayedColumns = ['AINS', 'ReviewId', 'Title', 'Content', 'Rate', 'Date' ];
+
+  public dataSource = new MatTableDataSource<Review>();
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+
+  constructor(private repositoryService: RepositoryService, private router: Router) { }
 
   ngOnInit(): void {
+    this.dataSource.filterPredicate = function (data, filter: string): boolean {
+      return data.content.toLowerCase().includes(filter);
+    };
+
+    this.loadData();
   }
 
+  loadData() {
+    this.repositoryService.getAllReviews("Reviews")
+      .subscribe(response => {
+        this.dataSource.data = response
+      });
+  }
+
+  ngAfterViewInit(): void {
+    this.dataSource.paginator = this.paginator;
+  }
+
+  public doFilter = (value: string) => {
+    this.dataSource.filter = value.trim().toLocaleLowerCase();
+  }
 }
+
